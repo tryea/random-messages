602ea8adeb870e121783f70ba9a4c96f506932c4
Ersapta Aristo <aristoersapta@gmail.com>
2021-06-18 09:59:40 +0800

    collect not boring question

---
+++ randomMessages.js
@@ -0,0 +1,12 @@
+const notBoringQuestion = [
+    'What’s your favorite way to spend a day off?',
+    'What type of music are you into?',
+    'What was the best vacation you ever took and why?',
+    'Where’s the next place on your travel bucket list and why?',
+    'What are your hobbies, and how did you get into them?',
+    'What was your favorite age growing up?',
+    'Was the last thing you read?',
+    'Would you say you’re more of an extrovert or an introvert?',
+    "What's your favorite ice cream topping?",
+    'What was the last TV show you binge-watched?'
+];
